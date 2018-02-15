36f65cdac49ceafe987e2139f1b034c52aba856c
shikha <shikha@shikhas-air>
2018-02-15 08:10:52 +0000

    Initial Commit

---
+++ BarCodeDemoCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarCodeDemoCode
+//
+//  Created by shikha  on 15/02/18.
+//  Copyright © 2018 shikha . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
